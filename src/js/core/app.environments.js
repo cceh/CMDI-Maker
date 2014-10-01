72a7b9c25cdb5780f4505cc7d9e7ed7bcc1a0301
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2014-10-01 17:35:49 +0200

    Changing error message to warning

--- src/js/core/app.environments.js
+++ src/js/core/app.environments.js
@@ -141,7 +141,7 @@ APP.environments = (function () {
 	my.create = function (environment){
 		
 		if (typeof environment == "undefined"){
-			console.error("APP.environments.create: Environment parameter is undefined!");
+			console.warn("APP.environments.create: Environment parameter is undefined!");
 			return;
 		}
 		
