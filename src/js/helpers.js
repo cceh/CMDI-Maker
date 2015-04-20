d167cb031587e56dd9cf5d005d034228216b2710
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2015-04-20 17:10:21 +0200

    More documentation

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -26,7 +26,17 @@ limitations under the License.
  * @class HELPERS
  */
 
- 
+
+/**
+ * Sends a HTTP POST via AJAX.
+ * @method postWithAJAX
+ * @param {string} url URL of the resource.
+ * @param {string} post_data Request body data as string.
+ * @param {function} success_callback Callback function to be called when there is a HTTP response. It gets the XMLHttpRequest object.
+ * @param {array} additional_headers Array with key-value-pairs, that are used as HTTP request headers.
+ * @return {Object} Returns the value of XMLHttpRequest.send()
+ * @static 
+ */	
 function postWithAJAX(url, post_data, success_callback, additional_headers){
 
 	var http = new XMLHttpRequest();
@@ -179,6 +189,15 @@ function readFileAsText(file, onsuccess){
 }
 
 
+
+/**
+ * Parses a JSON string to a JavaScript object. If JSON is invalid, the error is catched and a callback function is called. This is better than just relying on JSON.parse throwing real errors.
+ * @method parseJSON
+ * @param {string} string JSON string
+ * @param {function} onsuccess Function to be called when JSON could be parsed. Javascript Object is passed as parameter.
+ * @param {function} onerror Function to be called when JSON could not be parsed. No parameters are passed here.
+ * @static 
+ */	
 function parseJSON(string, onsuccess, onerror){
 
 	var object;
@@ -208,6 +227,14 @@ function parseJSON(string, onsuccess, onerror){
 };
 
 
+/**
+ * Reads the JSON content of a Javascript File Object and parses it.
+ * @method readFileAsJSON
+ * @param {Object} Javascript File Object to be parsed
+ * @param {function} onsuccess Function to be called when JSON could be parsed. Javascript Object is passed as parameter.
+ * @param {function} onerror Function to be called when JSON could not be parsed. No parameters are passed here.
+ * @static 
+ */	
 function readFileAsJSON(file, onsuccess, onerror){
 
 	readFileAsText(file, function(result){
@@ -217,6 +244,14 @@ function readFileAsJSON(file, onsuccess, onerror){
 }
 
 
+/**
+ * Loads a script from a URL and adds it dynamically to the webapp.
+ * @method addScript
+ * @param {string} url URL of the JS file
+ * @param {function} onloaded Function to be called when script is loaded and ready.
+ * @param {Boolean} async 
+ * @static 
+ */	
 function addScript(url, onloaded, async){
 
 	var script = document.createElement("script");
@@ -240,6 +275,12 @@ function addScript(url, onloaded, async){
 }
 
 
+/**
+ * Loads a CSS stylesheet file from a URL and adds it dynamically to the webapp.
+ * @method addStylesheet
+ * @param {string} url URL of the CSS file
+ * @static 
+ */	
 function addStylesheet(filename){
 
 	var fileref = document.createElement("link");
@@ -254,6 +295,12 @@ function addStylesheet(filename){
 }
 
 
+/**
+ * Adds either a JS file or a CSS file to the web page. This function is a wrapper of the two functions addScript and addStylesheet
+ * @method addFile
+ * @param {string} filename URL of the resource
+ * @static 
+ */	
 var addFile = function(filename){
 
 	if (strings.getFileTypeFromFilename(filename) == "js"){
@@ -267,6 +314,13 @@ var addFile = function(filename){
 };
 
 
+/**
+ * Adds files to the web page, which can be either JS or CSS. This function is a wrapper of the function addFile, which again is a wrapper of addScript and addStylesheet
+ * @method addFiles
+ * @param {Mixed} string_or_array String or array of strings with URL(s) of the resource(s)
+ * @param {String} url_prefix Prefix which is added to each passed URL
+ * @static 
+ */	
 var addFiles = function(string_or_array, url_prefix){
 
 	if (typeof string_or_array == "string"){
@@ -299,6 +353,15 @@ var addFiles = function(string_or_array, url_prefix){
 };
 
 
+/**
+ * Searches an array of objects. If an object is found, where object[key]==value, the function returns the index of the object in the array.
+ * @method getIndex
+ * @param {Array} array Array of Javascript Objects
+ * @param {String} key Key in object
+ * @param {String} value Value of key in object
+ * @return {Mixed} Returns index as number or undefined, if there is no such object.
+ * @static 
+ */	
 var getIndex = function(array, key, value){
 
 	for (var i=0; i < array.length; i++){
@@ -313,6 +376,14 @@ var getIndex = function(array, key, value){
 };
 
 
+/**
+ * Searches an array of objects. If an object is found, where object["id"]==value, the function returns the index of the object in the array.
+ * @method getIndexByID
+ * @param {Array} array Array of Javascript Objects
+ * @param {String} id Value of ID
+ * @return {Mixed} Returns index as number or undefined, if there is no such object.
+ * @static 
+ */	
 var getIndexByID = function(array, id){
 
 	return getIndex(array, "id", id);
@@ -320,6 +391,15 @@ var getIndexByID = function(array, id){
 };
 
 
+/**
+ * Searches an array of objects. If an object is found, where object[key]==value, the function returns the respective object.
+ * @method getObject
+ * @param {Array} array Array of Javascript Objects
+ * @param {String} key Key in object
+ * @param {String} value Value of key in object
+ * @return {Mixed} Returns an object or undefined, if there is no such object.
+ * @static 
+ */	
 var getObject = function(array, key, value){
 
 	for (var i=0; i < array.length; i++){
@@ -334,6 +414,14 @@ var getObject = function(array, key, value){
 };
 
 
+/**
+ * Searches an array of objects. If an object is found, where object["id"]==value, the function returns the the respective object.
+ * @method getIndexByID
+ * @param {Array} array Array of Javascript Objects
+ * @param {String} id Value of ID
+ * @return {Mixed} Returns an object or undefined, if there is no such object.
+ * @static 
+ */	
 var getObjectByID = function(array, id){
 
 	return getObject(array, "id", id);
@@ -341,7 +429,13 @@ var getObjectByID = function(array, id){
 };
 
 
-//iterates really through ALL items, even if some of them are deleted along the way
+/**
+ * Iterates through ALL items of an array. This function should be preferred over forEach, when the action could delete items of the array. I. e. this function iterates really through ALL items, even if some of them are deleted along the way.
+ * @method forAllItems
+ * @param {Array} array Any array
+ * @param {Function} action Action that is performed on each item.
+ * @static 
+ */	
 var forAllItems = function(array, action){
 
 	var i = array.length;
@@ -360,10 +454,18 @@ var forAllItems = function(array, action){
 	
 	}
 
-
 };
 
 
+
+/**
+ * Maps an array like Array.map()
+ * @method maps
+ * @param {Array} array Any array
+ * @param {Function} transform Transformation method that is performed on each item
+ * @return {Array} Returns the resulting array of the map. 
+ * @static 
+ */	
 var map = function(array, transform) {
 	var mapped = [];
 	
@@ -375,6 +477,14 @@ var map = function(array, transform) {
 };
 
 
+/**
+ * Creates an array with values by an array of JS objects. Of every object, the value of a specific key is taken.
+ * @method getArrayWithValuesByKey
+ * @param {Array} array Any array
+ * @param {String} key Key, of which the value is taken.
+ * @return {Array} Array with values.
+ * @static 
+ */	
 var getArrayWithValuesByKey = function(array, key){
 
 	var new_array = map(array, function(item){
@@ -386,6 +496,13 @@ var getArrayWithValuesByKey = function(array, key){
 };
 
 
+/**
+ * Creates an array with IDs by an array of JS objects. Of every object, the value of the key "id" is taken.
+ * @method getArrayWithIDs
+ * @param {Array} array Any array
+ * @return {Array} Array with IDs
+ * @static 
+ */	
 var getArrayWithIDs = function(array){
 
 	return getArrayWithValuesByKey(array, "id");
@@ -393,6 +510,14 @@ var getArrayWithIDs = function(array){
 };
 
 
+/**
+ * Filters an array like Array.filter()
+ * @method filter
+ * @param {Array} array Any array
+ * @param {Function} test Test function that is called for every array item. Array item is passed. If the function returns true, the item will appear in the array that is returned.
+ * @return {Array} Filtered array
+ * @static 
+ */	
 var filter = function(array, test) {
 	var passed = [];
 	for (var i = 0; i < array.length; i++) {
@@ -517,6 +642,7 @@ var g = function (id){
 
 /**
  * Remove an element and provide a function that inserts it into its original position
+ * @method removeToInsertLater
  * @param element {Element} The element to be temporarily removed
  * @return {Function} A function that inserts the element into its original position
  **/
@@ -534,7 +660,12 @@ var removeToInsertLater = function(element) {
 };
 
 
-function o(object, property_array){ 
+/**
+ * Remove an element and provide a function that inserts it into its original position
+ * @param element {Element} The element to be temporarily removed
+ * @return {Function} A function that inserts the element into its original position
+ **/
+function o(object, property_array){
 //returns value of object properties if they exist, if not returns ""
 
 	var value = object;
