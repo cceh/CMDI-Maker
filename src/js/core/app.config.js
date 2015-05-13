dc22bc53b39a3ff24ea9d371e12d37801c5bf181
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2015-05-13 14:15:14 +0200

    Build v2.12

--- src/js/core/app.config.js
+++ src/js/core/app.config.js
@@ -23,7 +23,7 @@ limitations under the License.
 APP.CONF = {
 
 	originator: "CMDI Maker by CLASS - Cologne Language Archive Services",    
-	version: "2.11",
+	version: "2.12",
 	LanguageCodePrefix: "ISO639-3:",
 	path_to_images: "img/",
 	path_to_icons: "img/icons/",
